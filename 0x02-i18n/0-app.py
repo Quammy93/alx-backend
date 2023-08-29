ac7870485755fb78667290b85278d69636b69f4d
Quammy93 <Quammy93@gmail.com>
2023-08-29 22:26:20 +0000

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
+""" Simple flask app."""
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app: Flask = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+        Return: 0-index.html
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5001)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
+""" Simple flask app."""
 
 from flask import Flask, render_template
 from flask_babel import Babel
+from config import Config
 
-app = Flask(__name__)
+
+app: Flask = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
-class Config:
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-app.config.from_object(Config)
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+        Return: 0-index.html
+    """
+    return render_template('1-index.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
-
+if __name__ == "__main__":
+    app.run(host="localhost", port=5001)
